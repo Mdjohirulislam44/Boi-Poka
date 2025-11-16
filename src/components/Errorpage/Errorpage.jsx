bec6b70764693fbfe38ef1e9a728cce362600cae
Md Johirul Islam <tusar8544@gmail.com>
2025-11-16 23:47:25 +0600

    navbar complite

---
+++ src/components/Errorpage/Errorpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Errorpage = () => {
+    return (
+        <div>
+            <h3 className='text-5xl'>page not found</h3>
+        </div>
+    );
+};
+
+export default Errorpage;
